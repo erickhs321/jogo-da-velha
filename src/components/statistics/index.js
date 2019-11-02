5b309bec4e3c9420648088606f6e233d98cec945
Erick <erickhenrique321@gmail.com>
2019-11-02 15:01:30 -0300

    style statistics

--- src/components/statistics/index.js
+++ src/components/statistics/index.js
@@ -23,17 +23,19 @@ export default function Statistics() {
             <p>jogador 2</p>
           </div>
         </div>
-        <div className="row">
-          <p className="time-value player-1">3.4s</p>
-          <p className="time-title">Tempo médio por jogada</p>
-          <p className="time-value player-2">10m30s</p>
+        <div className="container-time">
+          <div className="row">
+            <p className="time-value player-1">3.4s</p>
+            <p className="time-title">Tempo médio por jogada</p>
+            <p className="time-value player-2">10m30s</p>
+          </div>
+          <div className="row">
+            <p className="time-value player-1">3.4s</p>
+            <p className="time-title">Tempo total jogado</p>
+            <p className="time-value player-2">15m21s</p>
+          </div>
         </div>
-        <div className="row">
-          <p className="time-value player-1">3.4s</p>
-          <p className="time-title">Tempo total jogado</p>
-          <p className="time-value player-2">15m21s</p>
-        </div>
-        <div className="row">
+        <div className="row total-time">
           <p className="time-title">Tempo total geral</p>
           <p className="time-total">15m21s</p>
         </div>
