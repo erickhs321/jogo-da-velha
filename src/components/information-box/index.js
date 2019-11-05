640d05da8dc2b9f0efadadb508d89f51d7f1c1bb
Erick <erickhenrique321@gmail.com>
2019-11-05 01:01:14 -0300

    removing console.log

--- src/components/information-box/index.js
+++ src/components/information-box/index.js
@@ -6,7 +6,6 @@ export default function InformationBox(props) {
   const { winner, turn, draw } = props;
   return (
     <div id="information-box" className={(winner || draw) ? 'finish' : 'default'}>
-      {console.log({winner, draw})}
       {!winner && !draw && <span>Vez de: <span>{turn}</span></span>}
       {winner && !draw && <span>{winner} Venceu!</span>}
       {!winner && draw && <span>Empate!</span>}
